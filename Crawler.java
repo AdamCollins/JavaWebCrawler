dac4bbc5057db4464bb6d4f20387569ce70fdde2
Adam Collins <adamcollinssecure.com>
2015-10-31 12:27:28 -0700

    Creates new crawler object for pages found.

--- Crawler.java
+++ Crawler.java
@@ -3,40 +3,76 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Crawler {
-    static URL url;
-    static BufferedReader in;
+import static com.company.Main.createCrawler;
+
+public class Crawler
+{
+    private static URL url;
+    private static BufferedReader in;
+    static ArrayList<String> anchorsFound;
     Crawler(String site) throws Exception
     {
-
-       this.url =  new URL("https://www.reddit.com/");
+        this.url = new URL(site);
+        anchorsFound = new ArrayList<String>();
     }
 
-    public static void findAnchors() throws Exception {
+    public static void findAnchors() throws Exception
+    {
         in = new BufferedReader(new InputStreamReader(url.openStream()));
         String inputLine;
         String address = "";
 
-        while ((inputLine = in.readLine()) != null) {
-            for (int i = 0; i < inputLine.length() - 13; i++) {
-                if ("<a href=\"http".equals(inputLine.substring(i, i + 13))) {
+        while ((inputLine = in.readLine()) != null)
+        {
+            for (int i = 0; i < inputLine.length() - 13; i++)
+            {
+                if ("<a href=\"http".equals(inputLine.substring(i, i + 13)))
+                {
                     address = inputLine.substring(i + 9);
                     address = address.substring(0, address.indexOf("\""));
                     System.out.println(address);
-                } else if ("<A HREF=\"http".equals(inputLine.substring(i, i + 13))) {
+                    anchorsFound.add(address);
+                }
+                else
+                if ("<A HREF=\"http".equals(inputLine.substring(i, i + 13)))
+                {
                     address = inputLine.substring(i + 9);
                     address = address.substring(0, address.indexOf("\""));
                     System.out.println(address);
+                    anchorsFound.add(address);
                 }
             }
         }
         in.close();
+        //printAnchorsFound();
+        crawlAnchorsFound();
     }
 
-    public static void findSourceCode() throws Exception {
+    private static void crawlAnchorsFound() throws Exception {
+        for(int i = 0; i<anchorsFound.size(); i++)
+        {
+            createCrawler(anchorsFound.get(i));
+        }
+        anchorsFound = new ArrayList<String>();
+    }
+
+    private static void printAnchorsFound()
+    {
+        for(int i = 0; i<anchorsFound.size(); i++)
+        {
+            String anchor = anchorsFound.get(i);
+            System.out.println(anchor);
+        }
+    }
+
+    public static void findSourceCode() throws Exception
+    {
         String inputLine;
-        while ((inputLine = in.readLine()) != null) {
+        while ((inputLine = in.readLine()) != null)
+        {
             System.out.println(inputLine);
         }
 
--- Main.java
+++ Main.java
@@ -1,17 +1,30 @@
 package com.company;
+
 import java.net.*;
 import java.io.*;
-public class Main {
-    static URL startSite;
-    static BufferedReader in;
+import java.util.ArrayList;
 
-    public static void main(String[] args) throws Exception {
-        Crawler c = new Crawler("https://www.reddit.com/");
-        //findSourceCode();
-        c.findAnchors();
+public class Main
+{
+
+    static ArrayList<Crawler> crawlers;
+    public static int x;
+    public static void main(String[] args) throws Exception
+    {
+        crawlers = new ArrayList<Crawler>();
+        createCrawler("https://reddit.com/");
+
+        for (int i = 0; i < crawlers.size(); i++) {
+             Crawler c =  crawlers.get(i);
+            c.findAnchors();
+            //c.findSourceCode();
+        }
     }
 
-
-
+    public static void createCrawler(String url) throws Exception {
+        crawlers.add(new Crawler(url));
+        System.out.println(url);
+        if(Math.random()*100>95) System.out.println(crawlers.size());
+    }
 
 }
